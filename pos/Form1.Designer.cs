21591e6fcd0b16cd3bc3689fc4ce0bb3aa8d423d
saveen-maduranga <saveenonline@gmail.com>
2024-08-16 19:55:51 +0530

    Update Form1.Designer.cs

--- pos/Form1.Designer.cs
+++ pos/Form1.Designer.cs
@@ -32,6 +32,11 @@
             panel1 = new Panel();
             pictureBox1 = new PictureBox();
             label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
@@ -60,11 +65,62 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 30F, FontStyle.Bold);
-            label1.Location = new Point(44, 33);
+            label1.Location = new Point(48, 35);
             label1.Name = "label1";
             label1.Size = new Size(173, 54);
             label1.TabIndex = 1;
             label1.Text = "NexGen";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            label2.ForeColor = Color.DimGray;
+            label2.Location = new Point(78, 108);
+            label2.Name = "label2";
+            label2.Size = new Size(112, 28);
+            label2.TabIndex = 2;
+            label2.Text = "User Login";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10F);
+            label3.ForeColor = Color.DimGray;
+            label3.Location = new Point(99, 167);
+            label3.Name = "label3";
+            label3.Size = new Size(71, 19);
+            label3.TabIndex = 3;
+            label3.Text = "Username";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10F);
+            label4.ForeColor = Color.DimGray;
+            label4.Location = new Point(101, 246);
+            label4.Name = "label4";
+            label4.Size = new Size(67, 19);
+            label4.TabIndex = 4;
+            label4.Text = "Password";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.Gray;
+            textBox1.ForeColor = Color.White;
+            textBox1.Location = new Point(84, 207);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.Gray;
+            textBox2.ForeColor = Color.White;
+            textBox2.Location = new Point(84, 285);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 6;
             // 
             // Form1
             // 
@@ -72,6 +128,11 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             ClientSize = new Size(528, 399);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(panel1);
             ForeColor = SystemColors.Highlight;
@@ -91,5 +152,10 @@
         private Panel panel1;
         private PictureBox pictureBox1;
         private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private TextBox textBox2;
     }
 }
